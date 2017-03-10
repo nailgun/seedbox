5cf072c5641a5ba66c1646bf0edf70f55c261c12
Dmitry Bashkatov <dbashkatov@gmail.com>
2017-03-10 19:35:57 +0300

    remove obsolete

--- config.py
+++ config.py
@@ -3,9 +3,6 @@ import os
 basedir = os.path.dirname(__file__)
 cachedir = os.path.join(basedir, 'cache')
 
-# used to distigush unkown hosts and non-configured hosts
-boot_secret = '123'
-
 etcd_client_port = 2379
 etcd_peer_port = 2380
 credentials_path = '/etc/ssl/private'
