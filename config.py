5e7bd67fea20d80124637165d025d9a7871ffca9
Dmitry Bashkatov <dbashkatov@gmail.com>
2017-03-11 15:06:52 +0300

    move to config

--- config.py
+++ config.py
@@ -9,9 +9,12 @@ k8s_runtime = 'docker'  # or 'rkt'
 k8s_pod_network = '10.2.0.0/16'
 k8s_service_ip_range = '10.3.0.0/24'
 k8s_dns_service_ip = '10.3.0.10'
+k8s_hyperkube_tag = 'v1.5.2_coreos.0'
 
 etcd_client_port = 2379
 etcd_peer_port = 2380
+k8s_apiserver_insecure_port = 8080
+k8s_cluster_domain = 'cluster.local'
 cluster_credentials_path = '/etc/ssl/cluster'
 ca_cert_path = cluster_credentials_path + '/ca.pem'
 node_cert_path = cluster_credentials_path + '/node.pem'
