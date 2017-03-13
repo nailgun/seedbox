652c09c8f36fc573ee38ab0ccc144891946adc04
Dmitry Bashkatov <dbashkatov@gmail.com>
2017-03-13 23:30:40 +0300

    change admin title

--- src/seedbox/admin/__init__.py
+++ src/seedbox/admin/__init__.py
@@ -8,7 +8,7 @@ from .user import UserView
 from .credentials_data import CredentialsDataView
 
 
-admin = Admin(name='Cluster manager', template_mode='bootstrap3')
+admin = Admin(name='seedbox', template_mode='bootstrap3')
 admin.add_view(ClusterView(models.Cluster, models.db.session))
 admin.add_view(NodeView(models.Node, models.db.session))
 admin.add_view(UserView(models.User, models.db.session))
