e9d6a62a70f4f752b0834592122a0aa3efe4605a
Dmitry Bashkatov <dbashkatov@gmail.com>
2017-03-21 17:19:11 +0300

    kubelet service fix and simplify

--- src/seedbox/config_renderer/ignition/kubelet/__init__.py
+++ src/seedbox/config_renderer/ignition/kubelet/__init__.py
@@ -3,12 +3,11 @@ from seedbox.config_renderer.ignition.base import BaseIgnitionPackage
 
 
 class KubeletPackage(BaseIgnitionPackage):
-    def __init__(self, hyperkube_tag, hostname, is_schedulable, is_apiserver, runtime, apiserver_nodes, dns_service_ip, manage_etc_hosts):
+    def __init__(self, hyperkube_tag, hostname, is_schedulable, runtime, apiserver_nodes, dns_service_ip, manage_etc_hosts):
         self.template_context = {
             'hyperkube_tag': hyperkube_tag,
             'hostname': hostname,
             'is_schedulable': is_schedulable,
-            'is_apiserver': is_apiserver,
             'runtime': runtime,
             'apiserver_endpoints': ['https://{}:{}'.format(n.fqdn, config.k8s_apiserver_secure_port) for n in apiserver_nodes],
             'dns_service_ip': dns_service_ip,
