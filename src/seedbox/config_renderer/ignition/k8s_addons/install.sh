d32f6e47ab8d32cffa05780fa831089f708ef19e
Dmitry Bashkatov <dbashkatov@gmail.com>
2017-04-01 23:57:12 +0300

    k8s addons install script: improve readability

--- src/seedbox/config_renderer/ignition/k8s_addons/install.sh
+++ src/seedbox/config_renderer/ignition/k8s_addons/install.sh
@@ -3,31 +3,31 @@ set -e
 
 baseurl="http://127.0.0.1:{{ config.k8s_apiserver_insecure_port }}/"
 
-safeget() {
+strictget() {
     curl --fail --silent --show-error $@
 }
 
-safepost() {
+strictpost() {
     curl --fail --silent --show-error -H 'Content-Type: application/yaml' --data-binary $@
 }
 
 echo "Waiting for Kubernetes API..."
-until safeget "${baseurl}version"; do
+until strictget "${baseurl}version"; do
     sleep 5
 done
 
 echo "K8S addon: ClusterRoleBinding for default ServiceAccount in kube-system namespace"
-safepost @kube-system-default-sa.yaml "${baseurl}apis/rbac.authorization.k8s.io/v1alpha1/clusterrolebindings"
+strictpost @kube-system-default-sa.yaml "${baseurl}apis/rbac.authorization.k8s.io/v1alpha1/clusterrolebindings"
 
 echo "K8S addon: DNS"
-safepost @kube-dns-deployment.yaml "${baseurl}apis/extensions/v1beta1/namespaces/kube-system/deployments"
-safepost @kube-dns-svc.yaml "${baseurl}api/v1/namespaces/kube-system/services"
-safepost @kube-dns-autoscaler-deployment.yaml "${baseurl}apis/extensions/v1beta1/namespaces/kube-system/deployments"
+strictpost @kube-dns-deployment.yaml "${baseurl}apis/extensions/v1beta1/namespaces/kube-system/deployments"
+strictpost @kube-dns-svc.yaml "${baseurl}api/v1/namespaces/kube-system/services"
+strictpost @kube-dns-autoscaler-deployment.yaml "${baseurl}apis/extensions/v1beta1/namespaces/kube-system/deployments"
 
 echo "K8S addon: Heapster"
-safepost @heapster-deployment.yaml "${baseurl}apis/extensions/v1beta1/namespaces/kube-system/deployments"
-safepost @heapster-svc.yaml "${baseurl}api/v1/namespaces/kube-system/services"
+strictpost @heapster-deployment.yaml "${baseurl}apis/extensions/v1beta1/namespaces/kube-system/deployments"
+strictpost @heapster-svc.yaml "${baseurl}api/v1/namespaces/kube-system/services"
 
 echo "K8S addon: Dashboard"
-safepost @kube-dashboard-deployment.yaml "${baseurl}apis/extensions/v1beta1/namespaces/kube-system/deployments"
-safepost @kube-dashboard-svc.yaml "${baseurl}api/v1/namespaces/kube-system/services"
+strictpost @kube-dashboard-deployment.yaml "${baseurl}apis/extensions/v1beta1/namespaces/kube-system/deployments"
+strictpost @kube-dashboard-svc.yaml "${baseurl}api/v1/namespaces/kube-system/services"
