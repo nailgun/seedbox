f0560a4ee2a63f5dc503045dd46f78a16dcd03a5
Dmitry Bashkatov <dbashkatov@gmail.com>
2017-04-03 00:47:35 +0300

    move kubeconfig renderer to config_renderer package

--- src/seedbox/admin/user.py
+++ src/seedbox/admin/user.py
@@ -1,27 +1,11 @@
-import base64
-from collections import OrderedDict
-
-import yaml
-import yaml.resolver
 from flask import request, Response, flash, redirect
 from flask_admin import expose
 from flask_admin.actions import action
 from flask_admin.helpers import get_redirect_target
 from flask_admin.model.template import macro
 
-from seedbox import pki, kube, models
+from seedbox import pki, models, config_renderer
 from .base import ModelView
-
-
-class Dumper(yaml.SafeDumper):
-    pass
-
-
-def _dict_representer(dumper, data):
-    return dumper.represent_mapping(yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG, data.items())
-
-
-Dumper.add_representer(OrderedDict, _dict_representer)
 
 
 class UserView(ModelView):
@@ -64,57 +48,9 @@ class UserView(ModelView):
     @expose('/kubeconfig')
     def kubeconfig_view(self):
         user = self.get_one(request.args.get('id'))
-        user_creds = user.credentials
-        ca_creds = user.cluster.ca_credentials
-
-        kubeconfig = kube.get_kubeconfig(user.cluster.name,
-                                         user.cluster.k8s_apiserver_endpoint,
-                                         ca_creds.cert,
-                                         user.name,
-                                         user_creds.cert,
-                                         user_creds.key)
-        return Response(kubeconfig, mimetype='text/x-yaml')
+        return Response(config_renderer.kubeconfig.render([user]), mimetype='text/x-yaml')
 
     @action('kubeconfig', 'Get kubeconfig')
     def kubeconfig_action(self, ids):
-        clusters = {}
-        users = {}
-
-        for user in models.User.query.filter(models.User.id.in_(ids)):
-            users[user.name] = user
-            clusters[user.cluster.name] = user.cluster
-
-        contexts = [{
-            'name': user.name,
-            'context': {
-                'cluster': user.cluster.name,
-                'user': user.name,
-            },
-        } for user in users.values()]
-
-        clusters = [{
-            'name': cluster.name,
-            'cluster': {
-                'server': cluster.k8s_apiserver_endpoint,
-                'certificate-authority-data': base64.b64encode(cluster.ca_credentials.cert).decode('ascii'),
-            },
-        } for cluster in clusters.values()]
-
-        users = [{
-            'name': user.name,
-            'user': {
-                'client-certificate-data': base64.b64encode(user.credentials.cert).decode('ascii'),
-                'client-key-data': base64.b64encode(user.credentials.key).decode('ascii'),
-            },
-        } for user in users.values()]
-
-        config = OrderedDict([
-            ('apiVersion', 'v1'),
-            ('kind', 'Config'),
-            ('clusters', clusters),
-            ('users', users),
-            ('contexts', contexts),
-        ])
-
-        return Response(yaml.dump(config, default_flow_style=False, Dumper=Dumper),
-                        mimetype='text/x-yaml')
+        users = models.User.query.filter(models.User.id.in_(ids))
+        return Response(config_renderer.kubeconfig.render(users), mimetype='text/x-yaml')
---
+++ src/seedbox/config_renderer/kubeconfig.py
@@ -0,0 +1,59 @@
+import base64
+from collections import OrderedDict
+
+import yaml
+import yaml.resolver
+
+
+def render(users):
+    clusters_map = {}
+    users_map = {}
+
+    for user in users:
+        users_map[user.name] = user
+        clusters_map[user.cluster.name] = user.cluster
+
+    contexts = [{
+        'name': user.name,
+        'context': {
+            'cluster': user.cluster.name,
+            'user': user.name,
+        },
+    } for user in users_map.values()]
+
+    clusters = [{
+        'name': cluster.name,
+        'cluster': {
+            'server': cluster.k8s_apiserver_endpoint,
+            'certificate-authority-data': base64.b64encode(cluster.ca_credentials.cert).decode('ascii'),
+        },
+    } for cluster in clusters_map.values()]
+
+    users = [{
+        'name': user.name,
+        'user': {
+            'client-certificate-data': base64.b64encode(user.credentials.cert).decode('ascii'),
+            'client-key-data': base64.b64encode(user.credentials.key).decode('ascii'),
+        },
+    } for user in users_map.values()]
+
+    config = OrderedDict([
+        ('apiVersion', 'v1'),
+        ('kind', 'Config'),
+        ('clusters', clusters),
+        ('users', users),
+        ('contexts', contexts),
+    ])
+
+    return yaml.dump(config, default_flow_style=False, Dumper=Dumper)
+
+
+class Dumper(yaml.SafeDumper):
+    pass
+
+
+def _dict_representer(dumper, data):
+    return dumper.represent_mapping(yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG, data.items())
+
+
+Dumper.add_representer(OrderedDict, _dict_representer)
