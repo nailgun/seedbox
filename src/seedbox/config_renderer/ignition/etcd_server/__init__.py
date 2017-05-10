54b069f9ee2f1b70c6e1d93fce6e0621d90c9307
Dmitry Bashkatov <dbashkatov@gmail.com>
2017-05-10 21:02:08 +0300

    keep etcd configuration on persistent partition

--- src/seedbox/config_renderer/ignition/etcd_server/__init__.py
+++ src/seedbox/config_renderer/ignition/etcd_server/__init__.py
@@ -3,11 +3,16 @@ from seedbox.config_renderer.ignition.base import BaseIgnitionPackage
 
 class EtcdServerPackage(BaseIgnitionPackage):
     def get_units(self):
+        dropins = [
+            '40-etcd-cluster.conf',
+            '40-ssl.conf',
+            '40-oom.conf',
+            '30-image.conf',
+        ]
+
+        if self.node.persistent_partition:
+            dropins += ['40-persistent.conf']
+
         return [
-            self.get_unit_dropins('etcd-member.service', [
-                '40-etcd-cluster.conf',
-                '40-ssl.conf',
-                '40-oom.conf',
-                '30-image.conf',
-            ], enableunit=True),
+            self.get_unit_dropins('etcd-member.service', dropins=dropins, enableunit=True),
         ]
